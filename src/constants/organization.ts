0b9e50f91f493ec566719272beb4b1c879f8a4d9
thnhdtdev <thnhdat.dev@gmail.com>
2026-08-21 10:37:14 +0700

    ✨  feat: add Facebook channel alongside Zalo OA

--- src/constants/organization.ts
+++ src/constants/organization.ts
@@ -1,5 +1,5 @@
 export const NGAI_GIAO_ORGANIZATION = {
-   organization: {
+    organization: {
         name: "Trung tâm Phục vụ Hành chính công",
         fullName: "Trung tâm Phục vụ Hành chính công xã Ngãi Giao",
         locality: "Ngãi Giao",
@@ -11,8 +11,7 @@ export const NGAI_GIAO_ORGANIZATION = {
         },
 
         address: {
-            display:
-                "Số 70 đường Trần Hưng Đạo, xã Ngãi Giao, TPHCM, Việt Nam",
+            display: "Số 70 đường Trần Hưng Đạo, xã Ngãi Giao, TPHCM, Việt Nam",
             mapUrl: "https://maps.app.goo.gl/DfENcpKnLYf3LcP9A",
         },
     },
@@ -21,5 +20,11 @@ export const NGAI_GIAO_ORGANIZATION = {
         oaId: "2261565257434514638",
         defaultSupportMessage:
             "Xin chào, tôi cần hỗ trợ về thủ tục hành chính.",
+    },
+    social: {
+        facebook: {
+            url: "https://www.facebook.com/huyenchauductinhbariavungtau",
+            label: "Facebook",
+        },
     },
 };
